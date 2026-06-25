234c3a7f7d79fd7a9ef14c63c82ea93e7851656c
DaVor05 <davidvid1201@gmail.com>
2026-06-25 06:47:44 -0700

    Paso 1 a 3 Completado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,21 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'list-pokemons',
     pathMatch: 'full',
   },
+  {
+    path: 'list-pokemons',
+    loadComponent: () => import('./list-pokemons/list-pokemons.page').then( m => m.ListPokemonsPage)
+  },
+  {
+    path: 'detail-pokemon/:id',
+    loadComponent: () => import('./detail-pokemon/detail-pokemon.page').then( m => m.DetailPokemonPage)
+  },
+  {
+    path: '**',
+    redirectTo: 'list-pokemons',
+    pathMatch: 'full'
+  }
 ];
